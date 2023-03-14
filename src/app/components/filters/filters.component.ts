d585d8c37e6d63e2b4fb29dc7c160a70c8b4629a
Stéphane Menut <smenut@dawan.fr>
2023-03-14 11:58:49 +0100

    exercice slice

--- src/app/components/filters/filters.component.ts
+++ src/app/components/filters/filters.component.ts
@@ -9,9 +9,27 @@ export class FiltersComponent {
 
   date = new Date();
 
+  start = 0;
+  end = 5;
+
   studients = [
     "Soukaina", "Vanessa", "Bogdan", "Mahatoky", "Benjamin", "Jonathan", "Raphaël",
-    "Souleymane", "Sofiane", "Damien", "Samir", "Chaymae", "Anas", "Fatou", "Paul"
+    "Souleymane", "Sofiane", "Damien", "Samir", "Chaymae", "Anas", "Fatou", "Paul",
+    "Hugo", "Tarik"
   ];
+
+  public next(): void {
+    if(this.end < this.studients.length) {
+      this.start = this.start + 5;
+      this.end = this.end + 5;
+    }
+  }
+
+  public prev(): void {
+    if( this.start > 0 ) {
+      this.start = this.start - 5;
+      this.end = this.end - 5;
+    }
+  }
 
 }
